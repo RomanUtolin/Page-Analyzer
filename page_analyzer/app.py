c531c5b5a680c00ef972b5532f744ef02fee6a50
RomanUtolin <romautolin@gmail.com>
2023-03-30 21:04:57 +0300

    test

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
+DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
