83514ac7607387d3cd051db1ab779b0f05b2a28f
RomanUtolin <romautolin@gmail.com>
2023-04-18 22:29:20 +0300

    Refactor

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -17,55 +17,50 @@ def get_urls(conn):
         with conn.cursor(cursor_factory=extras.DictCursor) as curs:
             curs.execute(sql)
             url_from_bd = curs.fetchall()
-            app.app.logger.info('successful get_urls from bd')
+            app.app.logger.info('successful get all urls from bd')
             return url_from_bd
     except errors as error:
         app.app.logger.warning(error)
 
 
 def show_url(id_url, conn):
-    sql_for_url_name = 'SELECT id, name, created_at FROM urls WHERE id=%s'
-    sql_for_check_info = '''SELECT
-                        url_checks.id as check_id,
-                        status_code,
-                        h1,
-                        title,
-                        description,
-                        url_checks.created_at as site_check
-                        FROM urls
-                        JOIN url_checks ON urls.id = url_checks.url_id
-                        WHERE url_id = %s
-                        ORDER BY check_id DESC;'''
+    sql = '''SELECT
+             url_checks.id as check_id,
+             status_code,
+             h1,
+             title,
+             description,
+             url_checks.created_at as site_check
+             FROM urls
+             JOIN url_checks ON urls.id = url_checks.url_id
+             WHERE url_id = %s
+             ORDER BY check_id DESC;'''
+    url_from_bd = get_url_by_id(id_url, conn)
     try:
         with conn.cursor(cursor_factory=extras.DictCursor) as curs:
-            curs.execute(sql_for_url_name, (id_url,))
-            url_name = curs.fetchone()
-            curs.execute(sql_for_check_info, (id_url,))
+            curs.execute(sql, (id_url,))
             url_check_info = curs.fetchall()
-            app.app.logger.info('successful show_url from bd')
-            return url_name, url_check_info
+            app.app.logger.info(f'successful show_url_check_info id={id_url} from bd')
+            return url_from_bd, url_check_info
     except errors as error:
         app.app.logger.warning(error)
 
 
 def check_url(id_url, conn):
-    sql = 'SELECT name FROM urls WHERE id = %s;'
+    sql = '''INSERT INTO
+             url_checks (url_id,
+             created_at,
+             status_code,
+             description,
+             h1,
+             title)
+             VALUES (%s, %s, %s, %s, %s, %s)'''
     msg = {}
-    try:
-        with conn.cursor(cursor_factory=extras.DictCursor) as curs:
-            curs.execute(sql, (id_url,))
-            url = curs.fetchone()
-            app.app.logger.info('successful get url_name from bd')
-            seo_data = get_seo_data(url[0])
-            if seo_data:
-                sql = '''INSERT INTO
-                    url_checks (url_id,
-                                created_at,
-                                status_code,
-                                description,
-                                h1,
-                                title)
-                    VALUES (%s, %s, %s, %s, %s, %s)'''
+    url = get_url_by_id(id_url, conn)
+    seo_data = get_seo_data(url[0])
+    if seo_data:
+        try:
+            with conn.cursor(cursor_factory=extras.DictCursor) as curs:
                 curs.execute(sql,
                              (id_url,
                               datetime.now(),
@@ -76,50 +71,58 @@ def check_url(id_url, conn):
                 conn.commit()
                 msg['text'] = 'Страница успешно проверена'
                 msg['categories'] = 'success'
-                app.app.logger.info('successful check_url insert into bd')
+                app.app.logger.info(f'successful check_url id={id_url} insert into bd')
                 return msg
-    except errors as error:
-        app.app.logger.warning(error)
+        except errors as error:
+            app.app.logger.warning(error)
     msg['text'] = 'Произошла ошибка при проверке'
     msg['categories'] = 'danger'
     return msg
 
 
 def add_urls(url, conn):
-    sql_for_get_id = 'SELECT id FROM urls WHERE name=%s'
     sql_for_add = 'INSERT INTO urls (name, created_at) VALUES (%s, %s)'
     msg = {}
-    id_url = repeat(url, conn)
-    if id_url:
+    url_from_bd = get_id_url(url, conn)
+    if url_from_bd:
         msg['text'] = 'Страница уже существует'
         msg['categories'] = 'info'
-        return id_url, msg
+        return url_from_bd['id'], msg
     else:
         try:
             with conn.cursor(cursor_factory=extras.DictCursor) as curs:
                 curs.execute(sql_for_add, (url, datetime.now(),))
                 conn.commit()
-                app.app.logger.info(f'Successful select {url} from bd')
-                curs.execute(sql_for_get_id, (url,))
-                url_from_bd = curs.fetchone()
                 app.app.logger.info(f'Successful add {url} to bd')
+                url_from_bd = get_id_url(url, conn)
                 msg['text'] = 'Страница успешно добавлена'
                 msg['categories'] = 'success'
                 return url_from_bd['id'], msg
 
         except errors as error:
-            app.app.logger.info(f'Unsuccessful add {url} to bd', error)
+            app.app.logger.warning(error)
 
 
-def repeat(url, conn):
+def get_id_url(url, conn):
     sql = 'SELECT id FROM urls WHERE name=%s'
     try:
         with conn.cursor(cursor_factory=extras.DictCursor) as curs:
             curs.execute(sql, (url,))
             url_from_bd = curs.fetchone()
-            app.app.logger.info(f'Successful Check repeat {url} from bd')
-            if url_from_bd:
-                return url_from_bd['id']
+            app.app.logger.info(f'Successful get {url} from bd')
+            return url_from_bd
 
     except errors as error:
-        app.app.logger.info(f'Unsuccessful Check repeat {url} from bd', error)
+        app.app.logger.warning(error)
+
+
+def get_url_by_id(id_url, conn):
+    sql = 'SELECT name, id, created_at FROM urls WHERE id=%s'
+    try:
+        with conn.cursor(cursor_factory=extras.DictCursor) as curs:
+            curs.execute(sql, (id_url,))
+            url_from_bd = curs.fetchone()
+            app.app.logger.info(f'successful get {id_url} from bd')
+            return url_from_bd
+    except errors as error:
+        app.app.logger.warning(error)
